430235b63e94167e3b019a887ad85c8be4e04b31
libgober <libgober@g.harvard.edu>
2015-03-31 09:32:39 -0400

    debug

--- Text Processing/Module - Mar 27/handlers.py
+++ Text Processing/Module - Mar 27/handlers.py
@@ -162,7 +162,9 @@ def queuer(analyzer,headers=[],flattener=default_flattener,skip_list=[],save="./
                 case_id = case_number + str(year)
             
                 for row in output:
-                    writer.writerow(row + (citation,case_number,year))
+                    what_to_print = row + (citation,case_number,year)
+                    print(what_to_print)
+                    writer.writerow(what_to_print)
 
 
 
