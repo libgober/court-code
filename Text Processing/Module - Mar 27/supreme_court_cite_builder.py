3b65fd7298005399806e3fcb891c4fa5f9e8300d
libgober <libgober@g.harvard.edu>
2015-04-05 21:16:28 -0400

    died the missing P exclusion and minor typo on dallas

--- Text Processing/Module - Mar 27/supreme_court_cite_builder.py
+++ Text Processing/Module - Mar 27/supreme_court_cite_builder.py
@@ -38,7 +38,7 @@ def rewrite_values(tabulation):
     peters = re.compile("([0-9]{1,3})[\s\(]*([pP]et?e?r?s?)[.\s\)]*([0-9]{1,4})").search
     wheaton = re.compile("([0-9]{1,3})[\s\(]*([wW]he?a?t?o?n?)[.\s\)]*([0-9]{1,4})").search
     dallas = re.compile("([0-9]{1,3})[\s\(]*([dD]al?l?a?s?)[.\s\)]*([0-9]{1,4})").search
-    us = re.compile("([0-9](?![0-9]{0,2}\s*[uU][.\s]*[sS]*[.\s]*[0-9]{1,4}[.\s\)]*[cC|wW|bB|hH|wWdD])[0-9]{0,2})\s*([uU][.\s]*[sS]*)[.\s]*([0-9]{1,4})").search
+    us = re.compile("([0-9](?![0-9]{0,2}\s*[uU][.\s]*[sS]*[.\s]*[0-9]{1,4}[.\s\)]*[cC|wW|bB|hH|dD|pP]))\s*([uU][.\s]*[sS]*)[.\s]*([0-9]{1,4})").search
     
     #gather these functions into a list
     funcs = [dallas,cranch,wheaton,peters,howard,black,wallace,us]
